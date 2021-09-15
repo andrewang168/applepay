9919fb1595f4aedc5738ceadf19ad411883f1bfd
andrewang168 <andrew@ins.to>
2021-09-15 21:51:05 +0800

    update

--- app.js
+++ app.js
@@ -162,7 +162,7 @@ var applePayController = (function (uiController) {
     // Apple Pay Session from your Back-End
     appleSession.onvalidatemerchant = function (event) {
       _validateApplePaySession(event.validationURL, function (merchantSession) {
-        appleSession.completeMerchantValidation(merchantSession)
+        appleSession.completeMerchantValidation(JSON.parse(merchantSession))
       })
     }
 
